986e845e990430da4ae76de5976463f1bb11e63b
Tiwari <balramt02@gmail.com>
2026-05-28 17:37:21 +0200

    adding files for prompt chatbot code

--- prompt/chatbot.py
+++ prompt/chatbot.py
@@ -1,17 +1,20 @@
 from langchain_ollama import ChatOllama
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 
 model = ChatOllama( model="llama3.2:1b", temperature=0 )
 
-chat_history = []
+chat_history = [
+    SystemMessage(content= "YOu are helpfull AI assistant")
+]
 
 
 while True:
     user_input = input("You: ")
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content = user_input))
     if user_input == "Exit":
         break
 
     result = model.invoke(chat_history)
-    chat_history.append(result.content)
+    chat_history.append(AIMessage(content=result.content))
     print("AI: ", result.content)
